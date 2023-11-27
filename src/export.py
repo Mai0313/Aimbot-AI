b9ab129b6c121d17ea2e9475dd141b0178d9d657
Wei <mai@mai0313.com>
2023-11-27 21:07:01 +0800

    Add onnx to requirements.txt and remove yolov8_model_export from export.py

--- src/export.py
+++ src/export.py
@@ -14,7 +14,6 @@ console = Console()
 
 class PoseDetectionExport(BaseModel):
     yolov8_model_weights: str = Field(..., pattern=r".*\.pt$", frozen=True)
-    yolov8_model_export: str = Field(..., pattern=r".*\.pt$", frozen=True)
 
     yolov8_model_export_format: str = Field(
         default="onnx",
@@ -46,20 +45,17 @@ class PoseDetectionExport(BaseModel):
     def export_model(self):
         console.log(f"Saving Model in {self.yolov8_model_export_format}...")
         model = YOLO(self.yolov8_model_weights)
-        model = YOLO(self.yolov8_model_export)
         model_output_dir = model.export(format=self.yolov8_model_export_format)
         console.log(f"Model saved in {model_output_dir}")
 
 
 if __name__ == "__main__":
     config = OmegaConf.load("./configs/experiments/md1.yaml")
-    yolov8_model_weights = config.model.yolov8_model_weights
-    yolov8_model_export = config.model.yolov8_model_export
+    yolov8_model_weights = "./pretrained/yolov8x-pose-p6.pt"  # config.model.yolov8_model_weights
     yolov8_model_export_format = config.output_model.yolov8_model_export_format
 
     pose_detection_export = PoseDetectionExport(
         yolov8_model_weights=yolov8_model_weights,
-        yolov8_model_export=yolov8_model_export,
         yolov8_model_export_format=yolov8_model_export_format,
     )
     pose_detection_export.export_model()
